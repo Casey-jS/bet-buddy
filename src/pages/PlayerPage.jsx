b15ee70eb88d3783ac43a3a21de1709b3a37facf
Casey Sytsema <sytsemacj@gmail.com>
2022-12-07 21:43:16 -0500

    Finishes player view

--- src/pages/PlayerPage.jsx
+++ src/pages/PlayerPage.jsx
@@ -1,19 +1,30 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { Card, ListGroup, Button, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Card, ListGroup, Button, OverlayTrigger, Tooltip, Container, Row, Table } from "react-bootstrap";
 
 import NbaAPI from "../nbaAPI";
 import {GameLogHeader} from "../components/Headers";
-import {GameLogRow} from "../components/TableRows";
-import { Table } from "react-bootstrap";
 
 
-function GameLogTable(props){
 
-    return <Table striped bordered hover variant='dark' style={{width: "50%"}}><tbody><GameLogHeader />{
-        props.games.map((game, i) => (
-            <GameLogRow opp={game['opp']} result={game['result']} pts={game['pts']} fg={game['fg']} fg3={game['fg3']} ast={game['ast']} reb={game['reb']} stl={game['stl']} blk={game['blk']} key={i} />
-        ))
-    }</tbody></Table>
+function PlayerViewStats(props){
+    return (<Table striped bordered hover variant='dark' style={{marginTop: 40}}><tbody>
+        <tr><th>Team</th><th>Age</th><th>Games</th><th>Points</th><th>Assists</th><th>Rebounds</th><th>Steals</th><th>Blocks</th><th>FG%</th><th>3P%</th><th>FT%</th></tr>
+    <tr>
+        <td>{props.team}</td>
+        <td>{props.age}</td>
+        <td>{props.games}</td>
+        <td>{props.ppg}</td>
+        <td>{props.apg}</td>
+        <td>{props.rpg}</td>
+        <td>{props.spg}</td>
+        <td>{props.bpg}</td>
+        <td>{props.fgpct}</td>
+        <td>{props.fg3pct}</td>
+        <td>{props.ftpct}</td>
+        </tr>
+        </tbody></Table>
+        
+    );
 }
 
 export default function PlayerView({playerID}){
@@ -32,9 +43,7 @@ export default function PlayerView({playerID}){
     useEffect(getUser, [])
 
     const [player, setPlayer] = useState([]);
-    const [picture, setPicture] = useState("/");
-    const [gameLog, setGameLog] = useState([]);
-    
+    const [picture, setPicture] = useState("/");    
 
     // total player stats row
     let getPlayer = () => {
@@ -45,15 +54,6 @@ export default function PlayerView({playerID}){
         )
     }
     useEffect(getPlayer, []);
-
-    let getLog = () => {
-        api.getLastFiveGames(playerID).then(
-            logData => {
-                setGameLog(logData)
-            }
-        )
-    }
-    useEffect(getLog, []);
 
 
     const getHeadshot = useCallback(() => {
@@ -78,7 +78,9 @@ export default function PlayerView({playerID}){
 
 
     return (
-        <div className="position-relative" style={{display: "flex"}}>
+        <div style={{backgroundColor: "#8e9190", padding: "50px"}}>
+            <Container>
+                <Row className="justify-content-md-center">
             <Card className="shadow" border="info" style={{width: "25%", backgroundColor: '#8e9190', margin: "0"}}>
                 <Card.Img variant="top" src={picture} />
                 <Card.Body>  
@@ -102,8 +104,12 @@ export default function PlayerView({playerID}){
                 }
                 
             </Card>
-            <h1>Last 5 Games</h1>
-            <GameLogTable className="position-absolute top-0 end-0" games={gameLog} style={{marginLeft: "20px"}} />
+            </Row>
+            <Row>
+                <PlayerViewStats team={player['team']} age={player['age']} games={player['games']} ppg={player['ppg']} apg={player['apg']} rpg={player['rpg']} spg={player['spg']} bpg={player['bpg']} fgpct={player['fgpct']} fg3pct={player['fg3pct']} ftpct={player['ftpct']} />
+            </Row>
+            </Container>
+
             
         </div>
     )
