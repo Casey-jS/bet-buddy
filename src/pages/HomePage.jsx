d28cd0e8332dc2c66878b3448c7b7ab33d3c06fc
Casey Sytsema <sytsemacj@gmail.com>
2022-12-10 20:50:03 -0500

    Fixes headshots not loading

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,65 +19,32 @@ function StandingsTable(props){
     )
 }
 
-function StatCard({stat}){
+function StatCard(props){
+    console.log("Players passed into statcard: " + props.players)
 
-    const [players, setPlayers] = useState([]);
-    const [picture, setPicture] = useState("");
-    const [loading, setLoading] = useState(true);
-    const [playerID, setPlayerID] = useState(0);
+
 
     let stat_map = {
         "ppg" : "Points",
         "apg" : "Assists",
         "rpg" : "Rebounds",
-        "bpg" : "Blocks",
-        "fg3pct" : "Three %"
+        "bpg" : "Blocks"
     } 
-    let api = new NbaAPI();
-    const getPlayers = useCallback(() => {
-
-        api.fetch_top4(stat).then(
-            top4 => {
-                setPlayers(top4);
-                setPlayerID(top4[0]['id'])
-            }
-        )
-    }, [stat])
-    useEffect(() => {
-        getPlayers();
-    }, [getPlayers]);
-
-    
-
-    const getHeadshot = useCallback(() => {
-        api.fetchHeadshot(playerID).then(
-            imageBlob => {
-                setPicture(URL.createObjectURL(imageBlob)); // occupy image with img fetched
-                setLoading(false);
-            }
-        )
-      }, [playerID])
-    
-      useEffect(() => {
-        getHeadshot();
-      }, [getHeadshot]);
-
-    
     return <>
-        {loading ? <RingLoader /> :
+        
         <Card bg='dark' text='white' border="info" style={{width: "14rem"}}>
-            <Card.Img variant="top" src={picture} />
+            <Card.Img variant="top" src={props.headshot} />
             <Card.Body>
-                <Card.Title className="text-decoration-underline">{stat_map[stat]}</Card.Title>
-                <Card.Text>{players[0]['fullName'] + "  |  " + players[0][stat]}</Card.Text>
+                <Card.Title className="text-decoration-underline">{stat_map[props.stat]}</Card.Title>
+                <Card.Text>{props.players[0]['fullName'] + "  |  " + props.players[0][props.stat]}</Card.Text>
             </Card.Body>
             <ListGroup className="list-group-flush">
-                <ListGroup.Item variant='dark'>{players[1]['fullName'] + "   " + players[1][stat]}</ListGroup.Item>
-                <ListGroup.Item variant='dark'>{players[2]['fullName'] + "   " + players[2][stat]}</ListGroup.Item>
-                <ListGroup.Item variant='dark'>{players[3]['fullName'] + "   " + players[3][stat]}</ListGroup.Item>
+                <ListGroup.Item variant='dark'>{props.players[1]['fullName'] + "   " + props.players[1][props.stat]}</ListGroup.Item>
+                <ListGroup.Item variant='dark'>{props.players[2]['fullName'] + "   " + props.players[2][props.stat]}</ListGroup.Item>
+                <ListGroup.Item variant='dark'>{props.players[3]['fullName'] + "   " + props.players[3][props.stat]}</ListGroup.Item>
             </ListGroup>
         </Card>
-    }
+    
     </>
     
     
@@ -88,49 +55,92 @@ export default function HomePage(){
     let api = new NbaAPI();
     const [west, setWest] = useState([]);
     const [east, setEast] = useState([]);
+    const [players, setPlayers] = useState([]);
+    const [headShotIDs, setHeadShotIDs] = useState([]);
+    const [headShots, setHeadShots] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [count, setCount] = useState(0);
 
     let getWest = () => {
         api.get_west_standings().then(
             standings => {
                 setWest(standings);
+                console.log("Set the west standings")
             }
         )
     }
     useEffect(getWest, []);
 
-
     let getEast = () => {
         api.get_east_standings().then(
             standings => {
                 setEast(standings);
+                console.log("Set the east standings")
             }
         )
     }
     useEffect(getEast, []);
 
+    useEffect(() => {
+        if (count ===  4){
+            return
+        }
+        for (let i = 0; i < 4; i++){
+            console.log("Iteration #: " + i)
+            api.fetch_top4(stats[i]).then(
+                playerData => {
+                    setPlayers(current => [...current, playerData])
+                    setHeadShotIDs(current => [...current, playerData[0]['id']])
+                    setCount(count + 1);
+                    console.log("Count: " + count)    
+                }
+            )
+        }
+    }, [count]);
+
+
+
+    useEffect(() => {
+
+        if (count === 4){
+        
+            for (let i = 0; i < 4; i++){
+                console.log("Fetching player headshot for " + headShotIDs[i])
+                api.fetchHeadshot(headShotIDs[i]).then(
+                    imageBlob => {
+                        let image = URL.createObjectURL(imageBlob);
+                        setHeadShots(current => [...current, image]);
+                        
+                    }
+                )  
+                if (i == 3){
+                    setLoading(false);
+                } 
+            } 
+        }
+        
+    }, [count]);
 
     let stats = ["ppg", "apg", "rpg", "bpg"];
 
-    return (
+    return !loading && players[3] ? 
+                <div style={{backgroundColor: "#8e9190"}}>
+                <p className="text-center fs-1">League Leaders</p>
+                <Container style={{marginBottom: "10"}}>
+                    <Row>
+                        <Col>
+                            <CardGroup>
+                                {stats.map((stat, i) => (
+                                    <StatCard className="p-2" stat={stat} headshot={headShots[i]} players={players[i]} key={i}/>
+                                ))}
+                            </CardGroup>
+                        </Col>
+                    </Row>
+                    <p className="text-center fs-2">Standings</p>
+                    <Row>
+                        <Col><StandingsTable conf={"East"} teams={east} /></Col>
+                        <Col><StandingsTable conf={"West"} teams={west} /></Col>
+                    </Row>
 
-        <div style={{backgroundColor: "#8e9190"}}>
-        <p className="text-center fs-1">League Leaders</p>
-        <Container style={{marginBottom: "10"}}>
-            <Row>
-                <Col>
-                    <CardGroup>
-                        {stats.map((stat, i) => (
-                            <StatCard className="p-2" stat={stat} key={i}/>
-                        ))}
-                    </CardGroup>
-                </Col>
-            </Row>
-            <p className="text-center fs-2">Standings</p>
-            <Row>
-                <Col><StandingsTable conf={"East"} teams={east} /></Col>
-                <Col><StandingsTable conf={"West"} teams={west} /></Col>
-            </Row>
-
-        </Container></div>)
-
+                </Container></div> : <RingLoader />
 }
